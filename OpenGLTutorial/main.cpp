e7c1c751d5007c2323e26d3e43e3159da8e829db
dan tran <tsuoinc@gmail.com>
2020-07-12 21:35:12 -0400

    added sample Game class, Polygon class, and a Vertex Info structure containing vertex location, color, and texture index

--- OpenGLTutorial/main.cpp
+++ OpenGLTutorial/main.cpp
@@ -3,6 +3,7 @@
 
 #include <glad.h>
 #include <glfw3.h>
+#include <vector>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
@@ -10,22 +11,28 @@ void processInput(GLFWwindow* window);
 const char* vertexShaderSource = 
 				"#version 330 core\n"
 				"layout (location = 0) in vec3 aPos;\n"
+				"layout (location = 1) in vec3 aColor;\n"
+				"out vec3 ourColor;\n"
 				"void main()\n"
 				"{\n"
-				"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+				//"   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+				"	gl_Position = vec4(aPos, 1.0);\n"
+				"	ourColor = aColor;\n"
 				"}\0";
 
 const char* fragmentShaderSource =
 				"#version 330 core\n"
 				"out vec4 FragColor;\n"
+				// "uniform vec4 ourColor;\n" //using uniform
+				"in vec3 ourColor;\n"
 				"void main()\n"
 				"{\n"
-				"	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+				//"	FragColor = ourColor;\n"  //using uniform
+				"	FragColor = vec4(ourColor, 1.0);\n"
 				"}\n";
 
 
-int main()
-{
+int tutorial() {
 	glfwInit();
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
 	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
@@ -41,7 +48,7 @@ int main()
 	glfwMakeContextCurrent(window);
 
 
-	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) 
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
 	{
 		std::cout << "Failed to initialize GLAD" << std::endl;
 		return -1;
@@ -80,7 +87,13 @@ int main()
 		0, 1, 3,   // first triangle
 		1, 2, 3    // second triangle
 	};
-	
+
+	float vertices3[] = {
+		-0.2f, -0.9f, 0.0f, 1.0f, 0.0f, 0.0f,
+		 0.0f, -0.4f, 0.0f,	0.0f, 1.0f, 0.0f,
+		-0.1f, 0.8f, 0.0f,  0.0f, 0.0f, 1.0f
+	};
+
 	vertexShader = glCreateShader(GL_VERTEX_SHADER);
 	glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
 	glCompileShader(vertexShader);
@@ -110,13 +123,13 @@ int main()
 		glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
 		std::cout << "ERROR::SHADER::PROGRAM::LINK_FAILED\n" << infoLog << std::endl;
 	}
-	
+
 	glUseProgram(shaderProgram);
 
 	glDeleteShader(vertexShader);
 	glDeleteShader(fragmentShader);
 
-	
+
 
 	glGenVertexArrays(1, &VAO);
 	glBindVertexArray(VAO);
@@ -124,15 +137,22 @@ int main()
 	glGenBuffers(1, &VBO);
 	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	// use vertices1 for regular triangles drawing without indices
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices2), vertices2, GL_STATIC_DRAW);
+	//glBufferData(GL_ARRAY_BUFFER, sizeof(vertices2), vertices2, GL_STATIC_DRAW);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices3), vertices3, GL_STATIC_DRAW);
 
+	/*
 	glGenBuffers(1, &EBO);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+	*/
 
-	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
 	glEnableVertexAttribArray(0);
 
+	glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+
+	glUseProgram(shaderProgram);
 
 	// Testing Shaders functions
 	int nrAttributes;
@@ -144,23 +164,223 @@ int main()
 	{
 		processInput(window);
 
-		
+
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
-		glUseProgram(shaderProgram);
-		glBindVertexArray(VAO);
-		// glDrawArrays(GL_TRIANGLES, 0, 6);  // drawing triangles given verteces
 
+
+		glBindVertexArray(VAO);
+
+
+		/*
+		// Get uniform "ourColor"
+		float timeValue = glfwGetTime();
+		float greenValue = sin(timeValue) / 2.0f + 0.5f;
+		int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
+		glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+		*/
+
+		glDrawArrays(GL_TRIANGLES, 0, 3);  // drawing triangles given verteces
 		// drawing squares efficiently using indices
-		glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
-		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		//glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+		//glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
-	
+
 	glfwTerminate();
+	return 0;
+}
+
+struct VertexInfo {
+	float X;
+	float Y;
+	float Z;
+	float R;
+	float G;
+	float B;
+	float A;
+	float TextLocX;
+	float TextLocY;
+};
+
+class Polygon
+{
+private:
+	int numVertices;
+	VertexInfo* vertices;
+	int indices[];    // for EBO draw
+
+	void loadVertexData(VertexInfo* vertex, float x, float y, float z, float r, float g, float b, float a, float textx, float texty)
+	{
+		vertex->X = x;
+		vertex->Y = y;
+		vertex->Z = z;
+		vertex->R = r;
+		vertex->G = g;
+		vertex->B = b;
+		vertex->A = a;
+		vertex->TextLocX = textx;
+		vertex->TextLocY = texty;
+	}
+public:
+	Polygon(int n, int inds[], float locations[]) {
+		int locindex = 0;
+		numVertices = n;
+		vertices = new VertexInfo[n];
+		for (int i = 0; i < n; i++)
+		{
+			this->loadVertexData(&vertices[i], locations[locindex+0], locations[locindex+1], locations[locindex+2],
+								1.0f, 1.0f, 1.0f, 1.0f, 0.0f, 0.0f);
+			locindex += 3;
+		}
+	}
+	
+	Polygon(int n, int inds[], float locations[], float colors[])
+	{
+		numVertices = n;
+		vertices = new VertexInfo[n];
+		for (int i = 0; i < n; i++)
+		{
+			
+		}
+	}
+
+	Polygon(int n, int inds[], float locations[], float colors[], float textLocs[])
+	{
+		numVertices = n;
+		vertices = new VertexInfo[n];
+	}
+	
+	Polygon(int n, int inds[], VertexInfo verts[])
+	{
+		numVertices = n;
+		vertices = new VertexInfo[n];
+	}
+
+	void printVerticesInfo()
+	{
+		for (int i = 0; i < numVertices; i++)
+		{
+			std::cout << "Location: [" << vertices[i].X << "," << vertices[i].Y << "," << vertices[i].Z << "]" << std::endl;
+			std::cout << "Color: [" << vertices[i].R << "," << vertices[i].G << "," << vertices[i].B << "," << vertices[i].A << "]" << std::endl;
+			std::cout << "Texture Index: [" << vertices[i].TextLocX << "," << vertices[i].TextLocY << "]" << std::endl;
+		}
+	}
+};
+
+// class in charge of drawing onto window
+class Graphics
+{
+private:
+
+public:
+
+};
+
+class Game
+{
+private:
+	int width, height, framebufWidth, framebufHeight;
+	char* name;
+	GLFWwindow* window;
+
+public:
+	Game(int w, int h, char *n) {
+		width = w;
+		height = h;
+		framebufWidth = 0;
+		framebufHeight = 0;
+		name = n;
+
+		std::cout << sizeof(VertexInfo) << std::endl;
+		float f[] = {
+			0.2f, 0.4f, 0.0f,
+			0.5f, 0.7f, 0.0f,
+			0.7f, 0.7f, 0.0f
+		};
+		Polygon triangle(3, {}, f);
+		triangle.printVerticesInfo();
+
+		// init code and return window object
+		// requires private variables width, height, and name to be predefined
+		if ((window = initialize()) != NULL)
+		{
+			// game loop
+			while (!glfwWindowShouldClose(window))
+			{
+				update();
+				draw();
+				glfwPollEvents();
+			}
+		}
+		destroy();
+	}
+
+	// init and return a window object
+	GLFWwindow* initialize() {
+		glfwInit();
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+		glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+		glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+		GLFWwindow* window = glfwCreateWindow(width, height, name, NULL, NULL);
+		if (window == NULL)
+		{
+			std::cout << "Failed to create GLFW window << std::endl";
+			glfwTerminate();
+			return NULL;
+		}
+		glfwMakeContextCurrent(window);
+
+		if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+		{
+			std::cout << "Failed to initialize GLAD" << std::endl;
+			return NULL;
+		}
+
+		glfwGetFramebufferSize(window, &framebufWidth, &framebufHeight);
+		glViewport(0, 0, framebufWidth, framebufHeight);
+		std::cout << framebufWidth << std::endl << framebufHeight << std::endl;
+
+		glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+		return window;
+	}
+
+	void update() {
+		glfwGetFramebufferSize(window, &framebufWidth, &framebufHeight);
+		processInput(window);
+	}
+
+	void draw() {
+		glClearColor(0.0f, 0.4f, 0.5f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// draw triangle
+		// g.drawTriangle(triangle, 0, 0);
+
+		glfwSwapBuffers(window);
+	}
+
+	void destroy() {
+		glfwTerminate();
+	}
+
+	
+};
+
+int main()
+{
+    //return tutorial();
+	char name[] = "Game";
+	Game game(1200, 800, name);
+	
+
 	return 0;
 }
 
